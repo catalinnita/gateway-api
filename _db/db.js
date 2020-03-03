81d94790febd2c2b1f6727f0946de180a8ab5769
DESKTOP-P579E7L\catalin <catalin@incsub.com>
2020-03-03 10:56:20 +0000

    upgrade all packages

--- _db/db.js
+++ _db/db.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.CONNECTION_STRING, {'useNewUrlParser': true} );
+mongoose.connect(process.env.CONNECTION_STRING, { useUnifiedTopology: true } );
 mongoose.Promise = global.Promise;
 
 module.exports = {
